8246ab82925eb25a912506a34d02a40884e2c0af
PATHucd <140978146+PATHucd@users.noreply.github.com>
2023-07-30 13:13:37 -0700

    New serve file

--- server.R
+++ server.R
@@ -1,8 +1,10 @@
 shinyServer(function(input, output, session) {
 
+
+  
   data_query <- sharedServer("shared")
   station_map <- stationsServer("stations", data_query, parent = session)
   summary_data <- summaryServer("summary", data_query)
   tag_history <- tagServer("tag", parent = session)
-
+  callModule(about, "about")
 })
